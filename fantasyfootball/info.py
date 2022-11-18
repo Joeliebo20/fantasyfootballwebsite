11320d1dfa3bbca69594cf609e6677b33c363d2f
Joseph Lieberman <54378952+Joeliebo20@users.noreply.github.com>
2022-11-17 23:33:16 -0500

    Update info.py

--- fantasyfootball/info.py
+++ fantasyfootball/info.py
@@ -7,10 +7,8 @@ YEAR = 2022
 
 league = League(LEAGUE_ID, YEAR)
 
-league_mates = ["Joe", "Ryan", "Anthony", "Chris", "Jason", "Matt", 'Andrew', "Colin", "Paula", "Garrett"]
-owners = ["Joe Lieberman", "Ryan Lieberman", "Anthony I", "Chris Irving", "Big J", "Matt Lazz", "Andrew Howard", "colin mitchell", "paula lieberman", "Garrett Welch", "Michele Lazzara", "Scott Lazzara"]
+league_mates = ["Joe", "Ryan", "Anthony", "Chris", "Jason", "Matt", 'Andrew', "Colin", "Paula", "Garrett", 'Michele', 'Scott']
+owners = ["Joe", "Ryan", "Anthony", "Chris Irving", "Big J", "Matt", "Andrew", "colin mitchell", "paula lieberman", "Garrett Welch", "Michele Lazzara", "Scott Lazzara"]
 league_ids = [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
 teams = league.teams
 draft = league.draft
-
-
